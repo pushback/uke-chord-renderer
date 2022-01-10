67e6e008f62e2274e86ac2ea4448fc293343cfa5
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 07:00:40 +0900

    define chord A

--- src/chordLists/09_A.ts
+++ src/chordLists/09_A.ts
@@ -1,29 +1,28 @@
 import { ChordDefine } from '../chordDefine';
 
-// TODO: define chord
 export const CHORD_LISTS_A: ChordDefine[] = [
   // ---------------------------------
   // A
   // ---------------------------------
   {
     name: 'A',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 1, 2],
+    fingers: [0, 0, 1, 2]
   },
   {
     name: 'Am',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 0, 2],
+    fingers: [0, 0, 0, 2]
   },
   {
     name: 'A7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 1, 0],
+    fingers: [0, 0, 1, 0]
   },
   {
     name: 'Amaj7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 1, 1],
+    fingers: [0, 0, 2, 1]
   },
   {
     name: 'Am7',
@@ -32,42 +31,42 @@ export const CHORD_LISTS_A: ChordDefine[] = [
   },
   {
     name: 'A6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [4, 2, 4, 2],
+    fingers: [4, 2, 3, 1]
   },
   {
     name: 'Am6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [3, 2, 4, 2],
+    fingers: [3, 2, 4, 1]
   },
   {
     name: 'A9',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 0, 1, 0],
+    fingers: [2, 0, 1, 0]
   },
   {
     name: 'Am7b5',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [3, 3, 3, 2],
+    fingers: [4, 3, 2, 1]
   },
   {
     name: 'Aaug',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 1, 1, 2],
+    fingers: [0, 2, 1, 3]
   },
   {
     name: 'Adim',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [3, 2, 3, 2],
+    fingers: [4, 2, 3, 1]
   },
   {
     name: 'Asus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 2, 2],
+    fingers: [0, 0, 2, 1]
   },
   {
     name: 'A7sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 0, 2, 0],
+    fingers: [0, 0, 1, 0]
   }
 ];
