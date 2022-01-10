19e0e4dc2f8260b3626b3ec40dc97b37fe8ac6a1
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 05:27:04 +0900

    define chord D#/Eb

--- src/chordLists/03_D#Eb.ts
+++ src/chordLists/03_D#Eb.ts
@@ -1,73 +1,72 @@
 import { ChordDefine } from '../chordDefine';
 
-// TODO: define chord
 export const CHRORD_LISTS_DS_EF: ChordDefine[] = [
   // ---------------------------------
   // D#/Eb
   // ---------------------------------
   {
-    name: 'E#',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Eb',
+    flets: [1, 3, 3, 0],
+    fingers: [1, 3, 2, 0]
   },
   {
-    name: 'E#m',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebm',
+    flets: [1, 2, 3, 3],
+    fingers: [1, 2, 4, 3]
   },
   {
-    name: 'E#7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Eb7',
+    flets: [[3, 4], 3, 3, 3],
+    fingers: [[1, 2], 1, 1, 1]
   },
   {
-    name: 'E#maj7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebmaj7',
+    flets: [[3, 5], 3, 3, 3],
+    fingers: [[1, 3], 1, 1, 1]
   },
   {
-    name: 'E#m7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebm7',
+    flets: [4, 2, 3, 3],
+    fingers: [4, 1, 3, 2]
   },
   {
-    name: 'E#6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Eb6',
+    flets: [3, 3, 3, 3],
+    fingers: [1, 1, 1, 1]
   },
   {
-    name: 'E#m6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebm6',
+    flets: [3, 2, 3, 3],
+    fingers: [4, 1, 3, 2]
   },
   {
-    name: 'E#9',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Eb9',
+    flets: [1, 1, 1, 0],
+    fingers: [1, 1, 1, 0]
   },
   {
-    name: 'E#m7b5',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebm7b5',
+    flets: [4, 2, 3, 2],
+    fingers: [4, 2, 3, 1]
   },
   {
-    name: 'E#aug',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebaug',
+    flets: [2, 3, 3, 0],
+    fingers: [1, 3, 2, 0]
   },
   {
-    name: 'E#dim',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebdim',
+    flets: [3, 2, 3, 2],
+    fingers: [4, 2, 3, 1]
   },
   {
-    name: 'E#sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Ebsus4',
+    flets: [1, 4, 3, 3],
+    fingers: [1, 4, 3, 2]
   },
   {
-    name: 'E#7sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    name: 'Eb7sus4',
+    flets: [[3, 4], [3, 4], 3, 3],
+    fingers: [[1, 3], [1, 2], 1, 1]
   }
 ];
