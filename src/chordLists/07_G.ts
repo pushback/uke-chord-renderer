60bc4bad757e00a828c3a1ffb09d25e07e373c91
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 06:50:15 +0900

    define chord G

--- src/chordLists/07_G.ts
+++ src/chordLists/07_G.ts
@@ -1,73 +1,72 @@
 import { ChordDefine } from '../chordDefine';
 
-// TODO: define chord
 export const CHORD_LISTS_G: ChordDefine[] = [
   // ---------------------------------
   // G
   // ---------------------------------
   {
     name: 'G',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 3, 2, 0],
+    fingers: [3, 4, 2, 0]
   },
   {
     name: 'Gm',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 3, 2, 0],
+    fingers: [1, 3, 2, 0]
   },
   {
     name: 'G7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 1, 2, 0],
+    fingers: [3, 1, 2, 0]
   },
   {
     name: 'Gmaj7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 2, 2, 0],
+    fingers: [3, 2, 1, 0]
   },
   {
     name: 'Gm7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 2, 0],
+    fingers: [1, 1, 2, 0]
   },
   {
     name: 'G6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 0, 2, 0],
+    fingers: [4, 0, 3, 0]
   },
   {
     name: 'Gm6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 0, 2, 0],
+    fingers: [1, 0, 2, 0]
   },
   {
     name: 'G9',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 1, 2, 2],
+    fingers: [4, 1, 3, 2]
   },
   {
     name: 'Gm7b5',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [4, 3, 5, 3],
+    fingers: [3, 2, 4, 1]
   },
   {
     name: 'Gaug',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [2, 3, 3, 0],
+    fingers: [1, 3, 2, 0]
   },
   {
     name: 'Gdim',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 0, 1, 0],
+    fingers: [2, 0, 1, 0]
   },
   {
     name: 'Gsus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [3, 3, 2, 0],
+    fingers: [3, 2, 1, 0]
   },
   {
     name: 'G7sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [3, 1, 2, 0],
+    fingers: [3, 1, 2, 0]
   }
 ];
