297599439b56d4ecb322ac25f982dae78793a0f5
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 02:46:51 +0900

    Update chord list C

---
+++ src/chordLists/C.ts
@@ -0,0 +1,140 @@
+import { ChordDefine } from '../chordDefine';
+
+export const CHRORD_LISTS_C: ChordDefine[] = [
+  // ---------------------------------
+  // C
+  // ---------------------------------
+  {
+    name: 'C',
+    flets: [3, 0, 0, 0],
+    fingers: [3, 0, 0, 0]
+  },
+  {
+    name: 'Cm',
+    flets: [3, 3, 3, [3, 5]],
+    fingers: [1, 1, 1, [1, 3]]
+  },
+  {
+    name: 'C7',
+    flets: [1, 0, 0, 0],
+    fingers: [1, 0, 0, 0]
+  },
+  {
+    name: 'Cmaj7',
+    flets: [2, 0, 0, 0],
+    fingers: [2, 0, 0, 0]
+  },
+  {
+    name: 'Cm7',
+    flets: [3, 3, 3, 3],
+    fingers: [1, 1, 1, 1]
+  },
+  {
+    name: 'C6',
+    flets: [0, 0, 0, 0],
+    fingers: [0, 0, 0, 0]
+  },
+  {
+    name: 'Cm6',
+    flets: [3, 3, 3, 2],
+    fingers: [4, 3, 2, 1]
+  },
+  {
+    name: 'C9',
+    flets: [1, 0, 2, 0],
+    fingers: [1, 0, 2, 0]
+  },
+  {
+    name: 'Cm7b5',
+    flets: [3, 2, 3, 3],
+    fingers: [4, 1, 3, 2]
+  },
+  {
+    name: 'Caug',
+    flets: [3, 0, 0, 1],
+    fingers: [3, 0, 0, 1]
+  },
+  {
+    name: 'Cdim',
+    flets: [3, 2, 3, 2],
+    fingers: [4, 2, 3, 1]
+  },
+  {
+    name: 'Csus4',
+    flets: [3, 1, 0, 0],
+    fingers: [2, 1, 0, 0]
+  },
+  {
+    name: 'C7sus4',
+    flets: [1, 1, 0, 0],
+    fingers: [2, 1, 0, 0]
+  },
+  // ---------------------------------
+  // C#/Db
+  // ---------------------------------
+  {
+    name: 'C#',
+    flets: [[1, 4], 1, 1, 1],
+    fingers: [[1, 4], 1, 1, 1]
+  },
+  {
+    name: 'C#m',
+    flets: [5, 0, 1, 1],
+    fingers: [4, 0, 2, 1]
+  },
+  {
+    name: 'C#7',
+    flets: [[1, 2], 1, 1, 1],
+    fingers: [[1, 2], 1, 1, 1]
+  },
+  {
+    name: 'C#maj7',
+    flets: [[1, 3], 1, 1, 1],
+    fingers: [[1, 3], 1, 1, 1]
+  },
+  {
+    name: 'C#m7',
+    flets: [2, 0, 1, 1],
+    fingers: [3, 0, 2, 1]
+  },
+  {
+    name: 'C#6',
+    flets: [1, 1, 1, 1],
+    fingers: [1, 1, 1, 1]
+  },
+  {
+    name: 'C#m6',
+    flets: [1, 0, 1, 1],
+    fingers: [3, 0, 2, 1]
+  },
+  {
+    name: 'C#9',
+    flets: [[1, 2], 1, [1, 3], 1],
+    fingers: [[1, 2], 1, [1, 3], 1]
+  },
+  {
+    name: 'C#m7b5',
+    flets: [2, 0, 1, 0],
+    fingers: [2, 0, 1, 0]
+  },
+  {
+    name: 'C#aug',
+    flets: [0, 1, 1, 2],
+    fingers: [0, 2, 1, 3]
+  },
+  {
+    name: 'C#dim',
+    flets: [1, 0, 1, 0],
+    fingers: [2, 0, 1, 0]
+  },
+  {
+    name: 'C#sus4',
+    flets: [[1, 4], [1, 2], 1, 1],
+    fingers: [[1, 4], [1, 2], 1, 1]
+  },
+  {
+    name: 'C#7sus4',
+    flets: [[1, 2], [1, 2], 1, 1],
+    fingers: [[1, 3], [1, 2], 1, 1]
+  }
+];
