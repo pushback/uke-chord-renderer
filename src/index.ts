478c9e7ce4367554fbbe010569156b8854506979
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 01:02:01 +0900

    fix import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { argv } from 'node:process';
-import { ChordDetail } from './chordDetail';
-import { Renderer } from './renderer';
+import { ChordDefine } from './chordDetail';
+import { Renderer } from './chordRenderer';
 
 const chord = argv[2];
 const chordDetail = getChordDetail(chord);
 console.log(Renderer.getSVG(chordDetail));
 
-function getChordDetail (chord: string) : ChordDetail {
+function getChordDetail (chord: string) : ChordDefine {
   return {
     name: chord,
     flets: [3, 3, 3, [3, 5]],
