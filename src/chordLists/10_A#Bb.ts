7e33768766819a9935ae1d957bfc80ecd50ee00a
JhonSmith <jhonsmithworks@gmail.com>
2022-01-11 07:05:38 +0900

    define chord A#Bb

--- src/chordLists/10_A#Bb.ts
+++ src/chordLists/10_A#Bb.ts
@@ -1,73 +1,72 @@
 import { ChordDefine } from '../chordDefine';
 
-// TODO: define chord
 export const CHORD_LISTS_AS_BF: ChordDefine[] = [
   // ---------------------------------
   // A#/Bb
   // ---------------------------------
   {
     name: 'A#',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 2, 3],
+    fingers: [1, 1, 2, 3]
   },
   {
     name: 'A#m',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 1, [1, 3]],
+    fingers: [1, 1, 1, [1, 3]]
   },
   {
     name: 'A#7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, [1, 2], 1],
+    fingers: [1, 1, [1, 2], 1]
   },
   {
     name: 'A#maj7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [0, 1, 2, 3],
+    fingers: [0, 1, 2, 3]
   },
   {
     name: 'A#m7',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 1, 1],
+    fingers: [1, 1, 1, 1]
   },
   {
     name: 'A#6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 2, 0],
+    fingers: [1, 1, 2, 0]
   },
   {
     name: 'A#m6',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [4, 3, 5, 3],
+    fingers: [3, 2, 4, 1]
   },
   {
     name: 'A#9',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [[1, 3], 1, [1, 2], 1],
+    fingers: [[1, 3], 1, [1, 2], 1]
   },
   {
     name: 'A#m7b5',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 0, 1, 1],
+    fingers: [3, 0, 2, 1]
   },
   {
     name: 'A#aug',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 2, 2, 3],
+    fingers: [1, 3, 2, 4]
   },
   {
     name: 'A#dim',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 0, 1, 0],
+    fingers: [2, 0, 1, 0]
   },
   {
     name: 'A#sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, 3, 3],
+    fingers: [1, 1, 3, 2]
   },
   {
     name: 'A#7sus4',
-    flets: [0, 0, 0, 0],
-    fingers: [0, 0, 0, 0]
+    flets: [1, 1, [1, 3], 1],
+    fingers: [1, 1, [1, 3], 1]
   }
 ];
